8068d4337926e9d16084c0a2521658031b96f3ba
Barak Saidoff <barak.saidoff@gmail.com>
2020-01-26 14:23:16 -0500

    new react native app

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+// import StyleSheet from react-native when using StyleSheet.create() to define several style in one place
+
+export default class HelloWorldApp extends Component {
+  render() {
+
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text style ={{fontSize: 25}}>Hi Barak</Text>
+      </View> 
+    );
+  }
+}
+
+
