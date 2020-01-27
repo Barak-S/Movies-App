e9411dc57f6f0318611b3adda7661a6dba111c3d
Barak Saidoff <barak.saidoff@gmail.com>
2020-01-27 15:52:50 -0500

    ios home settup

--- App.js
+++ App.js
@@ -1,16 +1,80 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-// import StyleSheet from react-native when using StyleSheet.create() to define several style in one place
+  
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import axios from 'axios';
 
-export default class HelloWorldApp extends Component {
+
+class HomeScreen extends React.Component {
+
+  static navigationOptions = {
+    title: 'Home',
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+    headerLeft: () => (
+      <Button
+        onPress={() => alert('This is a button!')}
+        title="Menu"
+        color="#fff"
+      />
+    ),
+  };
+
   render() {
 
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style ={{fontSize: 25}}>Hi Barak</Text>
-      </View> 
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
     );
   }
 }
 
 
+class DetailsScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Details',
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
