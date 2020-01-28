04178337f8e5184b96c64ec4eafdc9938b27aded
Barak Saidoff <barak.saidoff@gmail.com>
2020-01-27 20:02:54 -0500

    End of Monday

--- App.js
+++ App.js
@@ -1,80 +1,134 @@
   
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { StyleSheet, Button, View, Text, TextInput, ScrollView, Image, TouchableHighlight, Modal } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import axios from 'axios';
+import MovieCard from './MovieCard'
 
-
-class HomeScreen extends React.Component {
-
-  static navigationOptions = {
-    title: 'Home',
-    headerStyle: {
-      backgroundColor: '#f4511e',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-    headerLeft: () => (
-      <Button
-        onPress={() => alert('This is a button!')}
-        title="Menu"
-        color="#fff"
-      />
-    ),
-  };
-
-  render() {
-
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-      </View>
-    );
-  }
-}
-
-
-class DetailsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Details',
-    headerStyle: {
-      backgroundColor: '#f4511e',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-  };
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-      </View>
-    );
-  }
-}
-
-const RootStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Details: DetailsScreen,
-  },
-  {
-    initialRouteName: 'Home',
-  }
-);
-
-const AppContainer = createAppContainer(RootStack);
+const apiUrl = "http://www.omdbapi.com/?apikey=a0514b1a"
+// const apiUrl = 'https://api.themoviedb.org/3/movie/550?api_key=c052d8400da4e25a04e8addd19e33290'
 
 export default class App extends React.Component {
-  render() {
-    return <AppContainer />;
+
+  state={
+    searchTerm: 'Enter a Movie...',
+    movies: [],
+    selectedMovie: {}
   }
+
+  handleSubmit=()=>{
+
+    fetch(`${apiUrl}&s=${this.state.searchTerm.split(" ").join("&")}`)
+    .then(resp=>resp.json())
+    .then(data => {
+      this.setState({
+        movies: data.Search
+      })
+    })
+  }
+
+  openPopup = id =>{
+    fetch(apiUrl+ "&i=" + id)
+    .then(resp=>resp.json())
+    .then(data=> {
+      this.setState({
+        selectedMovie: data
+      },()=>console.log(this.state.selectedMovie))
+    })
+  }
+
+  styles= StyleSheet.create({
+    container:{
+      flex: 1,
+      backgroundColor: '#FF851B',
+      alignItems: "center",
+      justifyContent: 'flex-start',
+      paddingTop: 65
+    },
+    title: {
+      color: '#fff',
+      fontSize: 32,
+      textAlign: "center",
+      marginBottom: 20
+    },
+    searchBox:{
+      fontSize: 21,
+      padding: 20,
+      width: '92%',
+      backgroundColor: '#fff',
+      borderRadius: 15,
+      marginBottom: 40
+    },
+    results: {
+      flex: 1,
+    },
+    popup: {
+      padding: 50,
+      backgroundColor: '#FF851B',
+      flex:1,
+    },
+    popTitle:{
+      fontSize: 29,
+      fontWeight: '600',
+      color: '#fff',
+    },
+    closeBtn:{
+      padding: 20,
+      fontSize: 24,
+      fontWeight: '600',
+      color: '#fff',
+      backgroundColor: '#445565'
+    }
+
+  })
+  
+  render() {
+    return (
+      <View style={this.styles.container}>
+        <Text style ={this.styles.title}>Movie Search</Text>
+        <TextInput
+          style={this.styles.searchBox}
+          placeholder={this.state.searchTerm}
+          onChangeText={text=> this.setState({searchTerm: text})}
+          onSubmitEditing={this.handleSubmit}
+        />
+        <ScrollView style={this.styles.results}>
+          {this.state.movies.map(movie=> (
+            <MovieCard
+              key={movie.imdbID}
+              movie={movie}
+              onPress={this.openPopup}
+            />)
+          )}
+
+        </ScrollView>
+        <Modal 
+          style={this.styles.modal}
+          animationType= "slide"
+          transparent={false}
+          visible={(typeof this.state.selectedMovie.Title != "undefined") ? true : false}
+        >
+          <View style={this.styles.popup}>
+            <Text style={this.styles.popTitle}>{this.state.selectedMovie.Title}</Text>
+            <Text style={{color: "#fff", fontSize: 20 }}>Rating: {this.state.selectedMovie.imdbRating}</Text>
+
+            <Image
+              source={{uri: this.state.selectedMovie.Poster}}
+              style={{width: '100%', height: 465, marginTop: 49}}
+              resizeMode= "cover"
+            />
+            <Text style={{color: "#fff", fontSize: 18, fontWeight: '450', marginTop: 20}}>{this.state.selectedMovie.Plot}</Text>
+          </View>
+          <TouchableHighlight
+            onPress={()=> this.setState({ selectedMovie : {} })}
+          >
+            <Text style={this.styles.closeBtn}>Close</Text>
+          </TouchableHighlight>
+        </Modal>
+        
+      </View> 
+    )
+  }
+
+  
 }
---
+++ MovieCard.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, TouchableHighlight, Image } from 'react-native';
+
+// import StyleSheet from react-native when using StyleSheet.create() to define several style in one place
+
+export default class MovieCard extends Component {
+
+  styles = StyleSheet.create({
+    result:{
+      flex: 1,
+      width: '100%',
+      marginBottom: 20
+    },
+    heading: {
+      color: '#fff',
+      fontSize: 18,
+      fontWeight: '600',
+      padding: 20,
+      backgroundColor: '#445565'
+    }
+  })
+
+  render() {
+    return (
+
+      <TouchableHighlight key = {this.props.key} onPress={()=> this.props.onPress(this.props.movie.imdbID)}>
+        <View style={this.styles.result}>
+          <Image
+            source={{uri: this.props.movie.Poster}}
+            style={{width: '100%', height: 450}}
+            resizeMode= "cover"
+          />
+          <Text style={this.styles.heading}>{this.props.movie.Title}</Text>
+        </View>
+      </TouchableHighlight>)
+
+    
+
+  }
+}
